d7e5af570b2a16df84e51f4b788d43d35e796861
Graham Clapham <graham.clapham@thomsonreuters.com>
2015-03-17 16:19:39 +0000

    Added gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+// Plumer handles errors - in the .pipe, naturally.
+var plumber = require('gulp-plumber');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('browserify', function(){
+    gulp.src('src/js/main.js')
+        .pipe(plumber())
+        .pipe(browserify({transform: 'reactify'}))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('copy', function(){
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('less', function () {
+    return gulp.src('./src/less/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['browserify', 'copy', 'less']);
+
+
+gulp.task('watch', function(){
+    gulp.watch('src/**/*.*', ['default']);
+});

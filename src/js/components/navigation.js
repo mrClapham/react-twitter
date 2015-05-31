08ee05992138fe7190378dd93df2c4c02c8bc280
Graham Clapham <info@gcdesign.co.uk>
2015-05-31 21:22:03 +0100

    Social icons added.

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -29,24 +29,17 @@ var Navigagtion = React.createClass({
                </div>
                <div id="navbar" className="navbar-collapse collapse">
                    <ul className="nav navbar-nav">
-                       <li className="active"><a href="#">Home</a></li>
+                       <li><a href="#">About </a></li>
                        <li><a href="#/users/">users</a></li>
                        <li><a href='/#/flikr/'> Flikr </a></li>
                        <li><a href='/#/experiments/'> Experiments </a></li>
 
                        <li><a href="#contact">Contact</a></li>
-                       <li className="dropdown">
-                           <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                           <ul className="dropdown-menu" role="menu">
-                               <li><a href="#">Action</a></li>
-                               <li><a href="#">Another action</a></li>
-                               <li><a href="#">Something else here</a></li>
-                               <li className="divider"></li>
-                               <li className="dropdown-header">Nav header</li>
-                               <li><a href="#">Separated link</a></li>
-                               <li><a href="#">One more separated link</a></li>
-                           </ul>
-                       </li>
+
+                           <li><a href="https://github.com/mrClapham" target="_blank"><div className="fa fa-github"></div> </a> </li>
+                           <li><a href="http://codepen.io/mrClapham/" target="_blank"><div className="fa fa-codepen"></div> </a> </li>
+                           <li><a href="https://www.linkedin.com/pub/graham-clapham/8/420/791" target="_blank"><div className="fa fa-linkedin"></div> </a> </li>
+                           <li><a href="mailto:info@gcdesign.co.uk" target="_blank"><div className="fa fa-envelope"></div> </a> </li>
                    </ul>
                </div>
            </div>
